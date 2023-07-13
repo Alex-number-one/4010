d4f8def5298a83936d6ddf8afe08b5840179b3ae
Alex <aleksejtatukov098@gmail.com>
2023-07-13 19:35:01 +0300

    task43

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double b1 = double.Parse(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = ( k1 * (b2 - b1) / (k1 - k2) ) + b1;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
