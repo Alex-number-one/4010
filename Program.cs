269c332fec9382c7c461dab7637e00b0d924ddc8
Alex <aleksejtatukov098@gmail.com>
2023-07-13 18:45:54 +0300

    task41

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int j = 0;
+int size = 10;
+int [] M = new int [size]; 
+for (int i = 0; i < size; i++ )
+{
+M [i] = Convert.ToInt32(Console.ReadLine());
+    if (M [i] > 0) 
+    {
+j++;
+    }
+
+}
+   Console.Write($"{string.Join(", ", M)}");
+   Console.Write(" -> " + j);
